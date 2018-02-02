4027547505ba65744c7cc593f187df1bd17317ae
sanjeev11111kumar <35979783+sanjeev11111kumar@users.noreply.github.com>
2018-02-02 16:38:51 +0000

    version 0.1 of my first python program.
    This is the hello world program for python. Initially, I will be using this as a template python program
    to start with other python programs that i will be writing.

---
+++ HelloWorld.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python ### Shebang line , to be used in unix & unix like environment
+
+###Comments
+##Script Comments
+
+#Title of the Program : The Hello World Python Program
+#Author of the program : Sanjeev Kumar
+#Date and Time of Creation : 02/02/2018 16:00 GMT
+#Version of the program : 0.1
+#Description : This is Sanjeev's first program in python. This program prints Hello World.
+#The version on the python used by Sanjeev is 2.7.6
+
+###Import Statements
+###Local Functions
+###Global Variables
+
+### Python statements
+##Code Comments
+
+# I/O statement to print Hello World on screen.
+# Print text can be included either in single or double quote
+# no line delimiter required
+# This is only one of the many styles of print statements.
+
+# version 2.x
+print "Hello World"
+
+#version 3.x
+#print ("Hello World")
